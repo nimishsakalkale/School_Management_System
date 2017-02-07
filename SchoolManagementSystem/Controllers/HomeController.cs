769f2293c87a057bdbb4538ccca70b9abd177b99
Nimish Sakalkale <nsakalk1@binghamton.edu>
2017-02-07 16:14:49 -0500

    Login Page added

--- SchoolManagementSystem/Controllers/HomeController.cs
+++ SchoolManagementSystem/Controllers/HomeController.cs
@@ -8,83 +8,15 @@ namespace SchoolManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        //Teset
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: Home/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Login(string username, string password)
         {
+
             return View();
-        }
-
-        // GET: Home/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Home/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Home/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Home/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Home/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Home/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
